4038755eaf90c334f9882a638d72493771aa3d91
Harsh Mavi <harshmavi2001@gmail.com>
2023-07-12 15:27:48 +0530

    pattern added

---
+++ p10.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,j,k;
+    cout<<"Enter the no. of rows:\n";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        k = 1;
+        for(j=0;j<n;j++)
+        {
+            if(n-j>i+1)
+            cout<<" ";
+            else
+            {
+                cout<<k;
+                k++;   
+            }
+        }
+        cout<<"\n";
+    }
+}
