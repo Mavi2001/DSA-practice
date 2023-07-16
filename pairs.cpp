f87a6f14649a0d9433be8314459ebaba617fdc5e
Harsh Mavi <harshmavi2001@gmail.com>
2023-07-16 19:31:58 +0530

    basics of pairs

---
+++ pairs.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int,string> p;
+    p = {5,"abcde"}; // or p = make_pair(5,"abcd"); to enter values inti the pair.
+    cout<<p.first<<" "<<p.second; //to print the values of the pair.
+}
