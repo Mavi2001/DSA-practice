1fd6ec6302b23893f2aeb60650e8681015a57c3a
Harsh Mavi <harshmavi2001@gmail.com>
2023-07-16 20:22:37 +0530

    array of vectors

---
+++ vector5.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int printvec(vector<int>&v)
+{
+    cout<<"The size of the vector is:"<<v.size()<<endl;
+    cout<<"The elements are:\n";
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" "<<endl;
+    }
+}
+int main()
+{
+    int N;
+    cin>>N;
+    vector<int> v[N];  //here we have an array of N vectors i.e., the elements of the array are N vectors.
+    cout<<"Enter the vectors:\n";
+    for(int i=0;i<N;i++)
+    {
+        cout<<"Enter the size of vector"<<i+1<<":\n";
+        int n;
+        cin>>n;
+        cout<<"Enter the elements of the vector:\n";
+        for(int j=0;j<n;j++)
+        {
+            int x;  //elements of the vector
+            cin>>x;
+            v[i].push_back(x);
+        }
+    }
+    for(int i=0;i<N;i++)
+    {
+        printvec(v[i]);
+    }
+    
+
+}
