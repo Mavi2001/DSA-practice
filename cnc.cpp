39514b3d74d41aadcfe3dfc28344685b6659d7d3
Harsh Mavi <harshmavi2001@gmail.com>
2023-07-17 21:26:29 +0530

    chef and chefina

---
+++ cnc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,x,y,p,q;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y>>p>>q;
+	    if(x+p*10 > y+q*10)
+	    {
+	        cout<<"Chefina"<<endl;
+	    }
+	    else if(x+p*10 < y+q*10)
+	    {
+	        cout<<"Chef"<<endl;
+	    }
+	    else
+	    cout<<"Draw"<<endl;
+	}
+	return 0;
+}
