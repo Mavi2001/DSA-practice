0d341bf08c37b55d38652365b017ba3b55c6428c
Harsh Mavi <harshmavi2001@gmail.com>
2023-07-21 19:48:33 +0530

    leetcode ques 81

---
+++ leet.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>nums = {2,5,6,0,0,1,2};
+    int target = 0;
+    
+    
+    int count=0;
+    
+    for(int i=0;i<nums.size();i++)
+    {
+        if(target == nums[i]);
+        count+=1;
+    }
+    if(count==0)
+    cout<<"false"<<endl;
+    else
+    cout<<"true"<<endl;
+    
+}
