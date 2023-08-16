96c662803bd780617d2372b9b6ec82c926a6f791
Harsh Mavi <harshmavi2001@gmail.com>
2023-08-16 18:35:10 +0530

    selection sort algorithm

---
+++ selectionsort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ss(int arr[], int n)
+{
+    for(int i=0 ; i<n-1 ; i++)
+    {
+        int minindex = i;
+        for(int j=i+1 ; j<n ;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                minindex = j;
+            }
+        }
+        swap(arr[i],arr[minindex]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0 ;i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    ss(arr,n);
+}
