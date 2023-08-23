0c0fdb8de798082ff6a8d22705b407f0a08c829e
Harsh Mavi <harshmavi2001@gmail.com>
2023-08-23 19:01:30 +0530

    Row with the maximum sum

---
+++ maxsumrow.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxsum(int arr[][3], int row, int col){
+    int sum = 0,maxsum = 0,r;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        {
+            sum += arr[i][j];
+        }
+        if(maxsum<sum){
+            maxsum = sum;
+            r = i;
+        }
+        sum = 0;
+    }
+    cout<<"Maximum sum is: "<<maxsum<<" at row "<<r + 1;
+}
+
+int main()
+{
+    cout<<"Enter the elements of the matrix: ";
+    int arr[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    maxsum(arr,3,3);
+}
